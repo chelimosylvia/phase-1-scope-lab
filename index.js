b06bf724d1fc027eb3a35bfaf8b9141afd330a9f
Sylviachelimo <chelimosylvia280@gmail.com>
2024-01-17 11:22:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+        console.log(customerName);
+    } else {
+        console.log("Customer name is not defined.");
+    }
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer);
+}
+const leastFavoriteCustomer = "Sylvia";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+}
+customerName();
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
